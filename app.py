ce4480cd0886a720a41b0d748f5e5d1bca52ebac
NicRios <n.rios.1997@gmail.com>
2019-09-10 10:36:24 -0600

    download csv

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def upload_route_summary():
                     justNum = '+' + justNum
                     row['Phone'] = justNum;
             print(index,row)
-    return "Success"
+    return render_template('download.html', data = data)
 
 @app.route("/format/json", methods=['GET', 'POST'])
 def format_json():
